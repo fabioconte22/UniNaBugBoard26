7bab2f35751617a0159d3189197bed774cbd0fc4
Fabio Conte <contefabio178@gmail.com>
2026-04-29 15:53:32 +0200

    feat: Aggiunto UserRepository e inizio JwtService

--- Backend/src/main/java/com/bugboard26/modules/auth/repository/UserRepository.java
+++ Backend/src/main/java/com/bugboard26/modules/auth/repository/UserRepository.java
@@ -2,11 +2,11 @@ package com.bugboard26.modules.auth.repository;
 
 import com.bugboard26.modules.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long>{
 
-    
+public interface UserRepository extends JpaRepository<User, String>{
+
+    Optional<User> findByEmail(String email);
     
 } 
---
+++ Backend/src/main/java/com/bugboard26/modules/auth/service/JwtService.java
@@ -0,0 +1,5 @@
+package com.bugboard26.modules.auth.service;
+
+public class JwtService {
+    
+}
