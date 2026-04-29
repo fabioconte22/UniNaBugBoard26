770c1bc456f5aaf2b74d26cbb18cfacf63cab105
Fabio Conte <contefabio178@gmail.com>
2026-04-29 08:34:25 +0200

    Creazione model User

--- Backend/src/main/java/com/bugboard26/modules/auth/model/User.java
+++ Backend/src/main/java/com/bugboard26/modules/auth/model/User.java
@@ -1,8 +1,36 @@
 package com.bugboard26.modules.auth.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    private String id; 
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
     private String nome; 
-    private String cognome;
+
+    @Column(nullable = false)
+    private String cognome; 
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password; 
+
+    @Enumerated(EnumType.STRING)
+    private Role role; // ADMIN || USER 
 
 }
