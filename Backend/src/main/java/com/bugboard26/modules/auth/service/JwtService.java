0082a3e9cb2549d90ab8ed62a57bc8ceace793b5
Fabio Conte <contefabio178@gmail.com>
2026-04-29 19:32:37 +0200

    chore: aggiunto application-example.yml

--- Backend/src/main/java/com/bugboard26/modules/auth/service/JwtService.java
+++ Backend/src/main/java/com/bugboard26/modules/auth/service/JwtService.java
@@ -1,5 +1,18 @@
 package com.bugboard26.modules.auth.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service 
 public class JwtService {
+    @Value("${jwt.secret")
+    private String secret; 
+
     
 }
