f9618a38f60d8202da0f68b0a5a292218d562e85
Fabio Conte <contefabio178@gmail.com>
2026-05-04 11:02:16 +0200

    feat: completato JwtService e aggiunto JwtAuthFilter

--- Backend/src/main/java/com/bugboard26/modules/auth/service/JwtService.java
+++ Backend/src/main/java/com/bugboard26/modules/auth/service/JwtService.java
@@ -11,8 +11,46 @@ import java.util.Date;
 
 @Service 
 public class JwtService {
-    @Value("${jwt.secret")
+    
+    @Value("${jwt.secret}")
     private String secret; 
+
+    @Value("${jwt.expiration-ms}")
+    private long expirationMs;
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, String email) {
+        return extractEmail(token).equals(email) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
 
     
 }
